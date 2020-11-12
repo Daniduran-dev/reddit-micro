a60e3c4eb9ea63e9a99d7afddf9762028c7d7a9d
RICKCOYL <rickoburu@gmail.com>
2020-11-12 11:10:54 +0300

    fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,18 @@
 class User < ApplicationRecord
+
   validates :name, presence: true, length: { maximum: 50 }
+
+
+
   validates :name, uniqueness: true
+
+
+
   has_many :post
+
+
+
   has_many :comment
+
 end
 
